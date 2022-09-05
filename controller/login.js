614ac707577218cf26cb78f7a1b917a4fd880082
Thandolwenkosi Magagula <106537445+tsgmagagul@users.noreply.github.com>
2022-09-05 12:02:21 +0200

    Add files via upload

--- controller/login.js
+++ controller/login.js
@@ -1,19 +1,28 @@
 //CRUD - Create, Read, Update Delete
-const loginModel = require('../models/loginmodels');
+const loginModel = require('../models/usermodels');
 const catchAsync = require('../utils/catchAsync');
 
-//CREATE USER
-exports.createLogin = catchAsync(async(req, res, next)=>{
-    const login = await loginModel.create(req.body);
-    //INSERT INTO [tablename] VALUES...
-    res.status(200).json({
-        status:"success",
-        message: "logined Successfully",
-        login
-    })
+
+exports.compareLogin = catchAsync(async(req,res,next)=>{
+    const {user_email,password}=req.body;
+const LoginData = await loginModel.findOne({where:{user_email}}).catch((err)=>{
+    console.log("Error:",err);
+})
+
+if(!LoginData)
+return res.json({message:"USER EMAIL OR PASSWORD DOES NOT MATCH "});
+
+if(LoginData.password !==password)
+return res.json({message:"USER EMAIL OR PASSWORD DOES NOT MATCH "});
+else{
+    return res.json({message:"loging was successfull "});
+}
+
+
 });
 
 
+
 exports.getlogin = catchAsync(async(req, res, next)=>{
-    const login  = await loginModel.findAll()
+ const LoginData  = await loginModel.findAll()
 });
