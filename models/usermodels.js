97a85a57792faa391dbd4ef8bfa1bcf9e3445321
Thandolwenkosi Magagula <106537445+tsgmagagul@users.noreply.github.com>
2022-09-08 09:42:11 +0200

    Add files via upload

--- models/usermodels.js
+++ models/usermodels.js
@@ -1,42 +1,67 @@
 const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs');
 const sequelize = require('../config/db');
+const { validateEmail } = require('../utils/validator');
+const { validationResult } = require('express-validator');
 const User = sequelize.define('user',{
+    
     user_id:{
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-    },
+      type: Sequelize.STRING,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+      
+      validate: 
+    {
+len:{args: [13,13],
+msg : 'Incorrrect ID number length',
+}
+    }
 
+    
+  },
     first_name:{
         type: Sequelize.STRING,
         allowNull: false,
+        errorMessage: "Firstname field cannot be empty",
     },
     last_name: {
         type: Sequelize.STRING,
         allowNull: false,
-      },
-      age: {
-        type: Sequelize.INTEGER, 
-        allowNull: false,
-
+        errorMessage: "Lastname field cannot be empty",
       },
       gender: {
         type: Sequelize.STRING,
         allowNull: false,
+        errorMessage: "Gender field cannot be empty",
       },
       user_type: {
         type: Sequelize.STRING,
         allowNull: false,
+        errorMessage:"User_Type field cannot be empty",
       },
       user_email: {
         type: Sequelize.STRING,
         allowNull: false,
-      },
-      password: {
+        unique: true,
+        isEmail:true,
+        
+             },
+    password: {
         type: Sequelize.STRING,
         allowNull: false,
+         validation: {
+        isLowercase:true,
+        isUppercase:true,
+        isAlphanumeric: true,
+        isNumeric:true,
+        max:16,
+        min:8,
       }
+    },
+ 
+          
+      
 } );
 
 module.exports = User;
