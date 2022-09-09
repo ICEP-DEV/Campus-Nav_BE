2bfffcad73b73154af2e83249a2fb12bcc739972
Thandolwenkosi Magagula <106537445+tsgmagagul@users.noreply.github.com>
2022-09-09 14:01:05 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 //2 REQUIRE ROUTES (WHICH WE DO NOT HAVE YET)
 const userRoute = require('./routes/userRoutes');
 const LoginRoute = require('./routes/loginRoutes');
+const DestinationRoute =require('./routes/destination.route');
 
 //3 APP.USE MIDDLEWARE
 app.use(express.json());
@@ -19,6 +20,7 @@ app.options('*', cors());
 //4 CREATE API URL
 app.use('/api/user', userRoute);
 app.use('/api/login', LoginRoute);
+app.use('/api/destination',DestinationRoute);
 
 
 app.use(globalErrHandler);
